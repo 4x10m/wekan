24c1c5834e3bb9d4574bd7771eab1819787efabb
evan <axiiom.home@gmail.com>
2021-11-15 23:25:11 +0100

    keep only docker compose stuff

--- models/server/createWorkbook.js
+++
@@ -1,5 +0,0 @@
-import Excel from 'exceljs';
-
-export const createWorkbook = function() {
-  return new Excel.Workbook();
-};
--- server/publications/tableVisibilityModeSettings.js
+++
@@ -1,3 +0,0 @@
-Meteor.publish('tableVisibilityModeSettings', function() {
-  return TableVisibilityModeSettings.find();
-});
